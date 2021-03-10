6ecf3052fdccbe0827879126d0216c7649e35a66
david <stojani_david@hotmail.com>
2021-03-10 01:51:48 +0100

    convex hull  and  animation

--- IdeaProjects/TSP/src/main/java/tsp_delaunay/ModifiedWeightedEdge.java
+++ IdeaProjects/TSP/src/main/java/tsp_delaunay/ModifiedWeightedEdge.java
@@ -3,28 +3,17 @@ package tsp_delaunay;
 import javafx.beans.property.ReadOnlyBooleanWrapper;
 import org.jgrapht.graph.DefaultEdge;
 
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
 public class ModifiedWeightedEdge extends DefaultEdge {
-    private final ReadOnlyBooleanWrapper inTour;
-    private final ReadOnlyBooleanWrapper inTriangulation;
-    private final ReadOnlyBooleanWrapper usefulDelaunayOrder;
-    private final ReadOnlyBooleanWrapper partOfConvexHull;
-    private final ReadOnlyBooleanWrapper inOptTour;
-    ReadOnlyBooleanWrapper inAugmentingCircle;
-    ReadOnlyBooleanWrapper inModifiedTriangulation;
-    String distanceToNextCircleCenterString;
-    Double leftDistance;
-    Double maxDistance;
-    Double rightDistance;
 
-    public ModifiedWeightedEdge(){
+
+    public ModifiedWeightedEdge() {
         super();
-        inTour = new ReadOnlyBooleanWrapper(false);
-        usefulDelaunayOrder = new ReadOnlyBooleanWrapper();
-        inTriangulation = new ReadOnlyBooleanWrapper(false);
-        partOfConvexHull = new ReadOnlyBooleanWrapper(false);
-        inOptTour = new ReadOnlyBooleanWrapper(false);
 
-        distanceToNextCircleCenterString = new String();
     }
 
 
